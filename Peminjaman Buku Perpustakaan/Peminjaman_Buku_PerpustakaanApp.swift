6ba9c8f83ef5ea7e5531e947e4c41f4be45547df
Reinaldo <reinaldo.lordianto.lewis@gmail.com>
2022-05-07 15:22:21 +0800

    Initial Commit

---
+++ Peminjaman Buku Perpustakaan/Peminjaman_Buku_PerpustakaanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peminjaman_Buku_PerpustakaanApp.swift
+//  Peminjaman Buku Perpustakaan
+//
+//  Created by Reinaldo Lordianto on 07/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Peminjaman_Buku_PerpustakaanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
